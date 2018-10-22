26ba7331a649fad3a6cedef566a96862d3fd035a
pinejh <pinejh95@gmail.com>
2018-10-22 18:26:59 -0400

    push remaining files

---
+++ server.js
@@ -0,0 +1,17 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var htmlPath = path.join(__dirname, 'app');
+
+app.use(express.static(htmlPath));
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
