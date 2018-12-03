aac544766ef06b7ab2b0ec94bb23e4dea760008b
pinejh <pinejh95@gmail.com>
2018-12-03 11:43:30 -0500

    fixed issue adding kd

--- app/js/main.js
+++ app/js/main.js
@@ -3,24 +3,18 @@ var cal = {};
 
 socket.on('emitCal', (newCal) => {
     cal = newCal;
-    vanillaCalendar.init({
-       disablePastDays: true
-    });
+    vanillaCalendar.init({ disablePastDays: true });
 });
-
-function promptInfo() {
-    document.getElementById('infoPopup').removeAttribute('hidden');
-}
 
 function submitInfo() {
     let name = document.getElementById('getName').value;
     let email = document.getElementById('getEmail').value;
     let date = document.getElementById('getDate').value;
 
-    if(confirm('Is the following information correct?\nName: '+name+'\nEmail: '+email+'\nDate: '+date)) {
-        socket.emit('inputKD', name, email, date);
-        window.location.href = window.location.origin + '/index.html';
-    }
+    //if(confirm('Is the following information correct?\nName: '+name+'\nEmail: '+email+'\nDate: '+date)) {
+    socket.emit('inputKD', name, email, date);
+    window.location.href = window.location.origin + '/index.html';
+    //}
 }
 
 function dateHasEvent(y, m, d) {
